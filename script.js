588c6e0d6f23d81f22470d3baf076b6def51b129
Diganta Nayak <diganta.19032001@gmail.com>
2022-12-21 21:11:17 +0530

    Done(styling excluded)

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
-const scrapeBtn = document.getElementById('btn');
+const scrapeBtn = document.getElementById("btn");
+const list = document.getElementById('emailList');
 
-scrapeBtn.addEventListener('click', () => {
-    alert('hi');
+// Handler to receive the emails
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    let emails = request.emails;
+    
+
+    //Display the items
+    if (emails == null || emails.length == 0) {
+        let li = document.createElement('li');
+        li.innerText = "No emails found";
+        list.appendChild(li);
+    } else {
+        emails.forEach((email) => {
+            let li = document.createElement('li');
+            li.innerText = email;
+            list.appendChild(li);
+        })
+    }
 })
+
+scrapeBtn.addEventListener("click", async () => {
+  //Get the current active tab
+
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  //Execute script to praise emails from page
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: scrapeEmailFromPage,
+  });
+});
+
+//Function to scrape email
+
+function scrapeEmailFromPage() {
+  const emailRegEx = /[\w\.=-]+@[\w\.-]+\.[\w]{2,3}/gim;
+
+  let emails = document.body.innerHTML.match(emailRegEx);
+
+    chrome.runtime.sendMessage({emails});
+}
